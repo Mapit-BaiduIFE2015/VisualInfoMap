0038b9b94234223495b3059cfdf42c330ffa0c5f
ironicstone <fang.expressway@gmail.com>
2015-07-30 09:56:46 +0800

    add colorlist

--- js/options.js
+++ js/options.js
@@ -85,7 +85,17 @@ var bar = {
 		"type": "bar",
 		'itemStyle': {                                
 			normal: {                                    
-				color: 'tomato',
+				// color: 'tomato',
+				color:function  (params) {					
+				   var colorList = [ '#C1232B', '#B5C334', '#FCCE10', '#E87C25', '#27727B',
+                                       '#FE8463', '#9BCA63', '#FAD860', '#F3A43B', '#60C0DD',
+                                       '#D7504B', '#C6E579'];
+                   if (colorList[params.dataIndex]) {
+                   	return colorList[params.dataIndex];
+                   }
+                   else
+                   	{return 'tomato' ;}
+				},
 				barBorderColor: 'tomato',
 				barBorderWidth: 1,
 				barBorderRadius: 5			                                
